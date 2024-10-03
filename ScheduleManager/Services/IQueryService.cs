201707776275735c52cb56b0376df375d39846a6
zuirseime <vovakibets88@gmail.com>
2024-10-03 14:08:25 +0300

    Added service interfaces

---
+++ ScheduleManager/Services/IQueryService.cs
@@ -0,0 +1,8 @@
+﻿namespace ScheduleManager.Services;
+
+public interface IQueryService<T> where T : class
+{
+    public Task<IEnumerable<T>> Sort(IEnumerable<T> entities);
+    public Task<IEnumerable<T>> Filter(IEnumerable<T> entities, Func<T, bool> predicate);
+    public Task<IEnumerable<T>> Find(IEnumerable<T> entities, string query);
+}
