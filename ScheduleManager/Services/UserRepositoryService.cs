8391b534784379caa61076b24f75de339d1809cb
zuirseime <vovakibets88@gmail.com>
2024-10-08 22:01:46 +0300

    Small changes in services

--- ScheduleManager/Services/UserRepositoryService.cs
+++ ScheduleManager/Services/UserRepositoryService.cs
@@ -1,29 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using ScheduleManager.Data.Models;
 using ScheduleManager.Data.Repositories;
-using System.Reflection;
+using System.Security.Claims;
 
 namespace ScheduleManager.Services;
 
-public class UserRepositoryService(UserManager<IdentityUser> manager)
+public class UserRepositoryService(UserRepository repository)
 {
-    public async Task<IEnumerable<IdentityUser>> GetAllAsync() 
-        => await manager.Users.ToListAsync();
-
-    public async Task<IdentityUser?> GetByIdAsync(string id) 
-        => await manager.FindByIdAsync(id);
-
-    public async Task<IdentityResult> CreateAsync(IdentityUser entity, string password) 
-        => await manager.CreateAsync(entity, password);
-
-    public async Task<IdentityResult> UpdateAsync(IdentityUser entity) => await manager.UpdateAsync(entity);
-
-    public async Task<IdentityResult?> DeleteAsync(string id)
-    {
-        var user = await manager.FindByIdAsync(id);
-        if (user is null) return null;
-
-        return await manager.DeleteAsync(user);
-    }
+    public async Task<IEnumerable<IdentityUser>> GetAllAsync() => await repository.GetAllAsync();
+    public async Task<IEnumerable<IdentityUser>> GetAllAsync(Func<IdentityUser, bool> predicate) => await repository.GetAllAsync(predicate);
+    public async Task<IdentityUser?> GetByIdAsync(Guid id) => await repository.GetByIdAsync(id);
+    public async Task<IdentityUser?> GetCurrent(ClaimsPrincipal user) => await repository.GetCurrent(user);
+    public async Task<IdentityResult?> CreateAsync(IdentityUser entity) => await repository.CreateAsync(entity);
+    public async Task<IdentityResult?> CreateAsync(IdentityUser entity, string password) => await repository.CreateAsync(entity, password);
+    public async Task<IdentityResult?> UpdateAsync(IdentityUser entity) => await repository.UpdateAsync(entity);
+    public async Task<IdentityResult?> DeleteAsync(IdentityUser entity) => await repository.DeleteAsync(entity);
 }
